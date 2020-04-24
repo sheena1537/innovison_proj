85f6f3d9ae52c6c13a5d611b326b5efb504df188
Pritam0209 <pritamraj0209@gmail.com>
2020-04-24 06:20:26 +0000

    added filtering part and updated master view template

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -2,11 +2,12 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-		"sap/ui/model/Filter",
+	"sap/ui/model/Filter",
 	"sap/ui/model/Sorter",
-	"sap/ui/model/FilterOperator"
-	
-], function (Controller, JSONModel, formatter, Filter, FilterOperator, Sorter) {
+	"sap/ui/model/FilterOperator",
+	'sap/ui/Device'
+
+], function (Controller, JSONModel, formatter, Filter, FilterOperator, Sorter,Device) {
 	"use strict";
 
 	return Controller.extend("com.sap.innovision.controller.Master", {
@@ -18,41 +19,42 @@ sap.ui.define([
 		 */
 		formatter: formatter,
 		onInit: function () {
-	// 			var oBotData = 
-	// {	bot: [{
-	// 		BotName: "Bot1",
-	// 		Status: "PASS",
-	// 		starttime: "start time: 23-08-2019 12:32:987"
-	// 	},
-	// 	{
-	// 	BotName: "Bot2",
-	// 		Status: "FAIL",
-	// 		starttime: "start time: 23-08-2019 12:32:987"
-	// 	},
-	// 	{
-	// 		BotName: "Bot3",
-	// 		Status: "PASS",
-	// 		starttime: "start time: 23-07-2019 12:32:987"
-	// 	}
-	// ]};
+			// 			var oBotData = 
+			// {	bot: [{
+			// 		BotName: "Bot1",
+			// 		Status: "PASS",
+			// 		starttime: "start time: 23-08-2019 12:32:987"
+			// 	},
+			// 	{
+			// 	BotName: "Bot2",
+			// 		Status: "FAIL",
+			// 		starttime: "start time: 23-08-2019 12:32:987"
+			// 	},
+			// 	{
+			// 		BotName: "Bot3",
+			// 		Status: "PASS",
+			// 		starttime: "start time: 23-07-2019 12:32:987"
+			// 	}
+			// ]};
 
+			// 	var oModel = new JSONModel(oBotData);
 
-	// 	var oModel = new JSONModel(oBotData);
-		
-	// 	this.getView().setModel(oModel,"A");
-	// 	console.log(oModel);
-this._oListFilterState = {
+			// 	this.getView().setModel(oModel,"A");
+			// 	console.log(oModel);
+			this._mViewSettingsDialogs = {};
+			this._oListFilterState = {
 				aFilter: [],
 				aSearch: []
 			};
-		
-		var oList = this.byId("list");
-					$.ajax({
+
+			var oList = this.byId("list");
+			$.ajax({
 				type: "GET",
 				dataType: "json",
 				url: "http://127.0.0.1:3005/output",
 				cors: true,
 				secure: true,
+				async: false,
 				headers: {
 					'Access-Control-Allow-Origin': '*'
 				},
@@ -62,60 +64,106 @@ this._oListFilterState = {
 					oModel.setData(data.robot.suite);
 					oList.setModel(oModel);
 					console.log('oModel');
-				console.log(oModel);
+					console.log(oModel);
 				}
 			});
 		},
-		
-		onSelectionChange: function(oEvent){
-			
+
+		onSelectionChange: function (oEvent) {
+			sap.ui.getCore().byId("container-innovision---app--myapp_fullscreen").setVisible(false);
+			sap.ui.getCore().byId("container-innovision---app--splitapp").setVisible(true);
 			var evnt = (oEvent.getParameter("listItem") || oEvent.getSource());
 			var botname = evnt.getBindingContext().getProperty("_attributes/source");
-		    var system_name = evnt.getBindingContext().getProperty("_attributes/name");
-		    system_name = (system_name.split('-'))[0];
-			botname = (botname.split('\\')).slice(-2,-1);
+			var system_name = evnt.getBindingContext().getProperty("_attributes/name");
+			system_name = (system_name.split('-'))[0];
+			botname = (botname.split('\\')).slice(-2, -1);
 			//console.log(botname[0]);
 			var oR = sap.ui.core.UIComponent.getRouterFor(this);
-			oR.navTo("Detail_r", {"botid": botname[0], "sysid": system_name});
+			oR.navTo("Detail_r", {
+				"botid": botname[0],
+				"sysid": system_name
+			});
 		},
-		
-		onSearch : function (evt) {
-	// create model filter
-	var filters = [];
-	var query = evt.getParameter("query");
-	console.log("query");
-	if (query && query.length > 0) {
-		var filter = new sap.ui.model.Filter("_attributes/source", sap.ui.model.FilterOperator.Contains, query);
-		filters.push(filter);
-	}
-	
 
-	// update list binding
-	var list = this.getView().byId("list");
-	
-	var binding = list.getBinding("items");
-	binding.filter(filters);
-},
+		onSearch: function (evt) {
+			// create model filter
+			var filters = [];
+			var query = evt.getParameter("query");
+			console.log("query");
+			if (query && query.length > 0) {
+				var filter = new sap.ui.model.Filter("_attributes/source", sap.ui.model.FilterOperator.Contains, query);
+				filters.push(filter);
+			}
 
-onSort: function(evt){
-	
-	var oView = this.getView();
-        	var oList = this.byId("list");
-        var oBinding = oList.getBinding("items");
+			// update list binding
+			var list = this.getView().byId("list");
 
-        var SORTKEY = "status/_attributes/starttime";
-        var DESCENDING = false;
-        var GROUP = false;
-        var aSorter = [];
+			var binding = list.getBinding("items");
+			binding.filter(filters);
+		},
 
-        aSorter.push(new sap.ui.model.Sorter(SORTKEY, DESCENDING, GROUP));
-        oBinding.sort(aSorter);
-        
-        var oModel = this.getView().getModel();
-        oModel.refresh();
-	
-}
-       
+		onSort: function (evt) {
+
+			var oView = this.getView();
+			var oList = this.byId("list");
+			var oBinding = oList.getBinding("items");
+
+			var SORTKEY = "status/_attributes/starttime";
+			var DESCENDING = false;
+			var GROUP = false;
+			var aSorter = [];
+
+			aSorter.push(new sap.ui.model.Sorter(SORTKEY, DESCENDING, GROUP));
+			oBinding.sort(aSorter);
+
+			var oModel = this.getView().getModel();
+			oModel.refresh();
+
+		},
+		createViewSettingsDialog: function (sDialogFragmentName) {
+			var oDialog = this._mViewSettingsDialogs[sDialogFragmentName];
+
+			if (!oDialog) {
+				oDialog = sap.ui.xmlfragment(sDialogFragmentName, this);
+				this._mViewSettingsDialogs[sDialogFragmentName] = oDialog;
+
+				if (Device.system.desktop) {
+					oDialog.addStyleClass("sapUiSizeCompact");
+				}
+			}
+			return oDialog;
+		},
+		handleFilterButtonPressed: function () {
+			console.log("filtering");
+			this.createViewSettingsDialog("com.sap.innovision.view.FilterDialog").open();
+		},
+		handleFilterDialogConfirm: function (oEvent) {
+			console.log("inside filtering");
+			var mParams = oEvent.getParameters(),
+				aFilters = [];
+			// 	console.log(oEvent);
+			// console.log(mParams);
+			mParams.filterItems.forEach(function (oItem) {
+				// console.log(oItem.oParent.getId());
+				var ParentKey = oItem.getParent().getKey();
+				var aSplit = oItem.getKey(),
+					sPath = aSplit,
+					oFilter;
+				if (ParentKey === "STATUS")
+					oFilter = new sap.ui.model.Filter("status/_attributes/status", sap.ui.model.FilterOperator.Contains, sPath);
+				else if (ParentKey === "SYSTEM")
+					oFilter = new sap.ui.model.Filter("_attributes/name", sap.ui.model.FilterOperator.Contains, sPath);
+				aFilters.push(oFilter);
+			});
+
+			var list = this.getView().byId("list");
+
+			var binding = list.getBinding("items");
+			binding.filter(aFilters);
+
+			// apply filter settings
+		}
+
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
